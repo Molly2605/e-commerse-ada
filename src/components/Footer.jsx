cf4efd1cdf0c5c7af11bc74347afec41a73ea04b
Molly <mili_lasschar@hotmail.com>
2025-02-21 20:51:33 -0300

    arreglo de footer, link de repo + link de pergil de github

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Box, Container, Text, Link } from "@chakra-ui/react";
+import { Box, Container, Text, Link, Flex } from "@chakra-ui/react";
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -7,9 +8,17 @@ const Footer = () => {
       <Container maxW="container.xl" textAlign="center">
         <Text>2025 Ecommerce para ADA IT Woman</Text>
         <Text mt={2}>
-          Hecho por Molly ❤️
-          <Link href="https://github.com/Molly2605" color="white" fontWeight="bold" isExternal>
-            GitHub
+          Hecho por Molly{" "}
+          <Flex align="center" display="inline-flex">
+            ❤️
+            <Link href="https://github.com/Molly2605" color="white" fontWeight="bold" isExternal ml={2}>
+              <FaGithub size={24} />
+            </Link>
+          </Flex>
+        </Text>
+        <Text mt={2}>
+          <Link href="https://github.com/Molly2605/e-commerse-ada" color="white" fontWeight="bold" isExternal>
+            Repositorio en Github
           </Link>
         </Text>
       </Container>
