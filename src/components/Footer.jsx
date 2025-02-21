f99db58e151e974307c42749fd47cab408f59356
Molly <mili_lasschar@hotmail.com>
2025-02-21 20:55:09 -0300

    arreglo de footer, p dentro de un div.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <Box bg="black" color="white" py={10}>
       <Container maxW="container.xl" textAlign="center">
         <Text>2025 Ecommerce para ADA IT Woman</Text>
-        <Text mt={2}>
+        <Box mt={2}>
           Hecho por Molly{" "}
           <Flex align="center" display="inline-flex">
             ❤️
@@ -15,7 +15,7 @@ const Footer = () => {
               <FaGithub size={24} />
             </Link>
           </Flex>
-        </Text>
+        </Box>
         <Text mt={2}>
           <Link href="https://github.com/Molly2605/e-commerse-ada" color="white" fontWeight="bold" isExternal>
             Repositorio en Github
